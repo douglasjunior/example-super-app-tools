b302bbae48ce76715a599d03f5be846279eb47ec
Douglas Nassif Roma Junior <douglas.junior@db1.com.br>
2022-06-24 17:30:44 -0300

    initial commit

---
+++ src/sync-with-core.js
@@ -0,0 +1,124 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+const path = require('path');
+const fetch = require('node-fetch');
+const chokidar = require('chokidar');
+const packageJson = require('../package.json');
+
+class SyncError extends Error {
+  constructor(message) {
+    super(message);
+  }
+}
+
+const FATAL_ERROR = new SyncError(
+  'Não foi possível conectar, certifique-se de que o metro está em execução no "core" e escutando na porta 8081.',
+);
+
+function ensureDirectoryExistence(dirPath) {
+  const dirname = path.dirname(dirPath);
+  if (!fs.existsSync(dirname)) {
+    fs.mkdirSync(dirname, {recursive: true});
+  }
+}
+
+function validateProcessDirectory(processDir) {
+  if (!fs.existsSync(path.resolve(processDir, 'package.json'))) {
+    throw new SyncError(
+      'Certifique-se de executar o comando "yarn sync-with-core" na raiz do projeto.',
+    );
+  }
+}
+
+function startSync(corePath) {
+  const modulePath = process.cwd();
+  const linkedModulePath = path.resolve(
+    corePath,
+    'node_modules',
+    packageJson.name,
+  );
+
+  validateProcessDirectory(modulePath);
+
+  function getLinkedFilePath(originalPath) {
+    return originalPath.replace(modulePath, linkedModulePath);
+  }
+
+  function copyAddedOrChangedFile(originalPath) {
+    const tempUpdatedPath = getLinkedFilePath(originalPath);
+    ensureDirectoryExistence(tempUpdatedPath);
+    fs.copyFileSync(originalPath, tempUpdatedPath);
+  }
+
+  function unlinkDeletedFile(originalPath) {
+    const tempUpdatedPath = getLinkedFilePath(originalPath);
+    fs.unlinkSync(tempUpdatedPath);
+  }
+
+  const watcher = chokidar.watch(modulePath, {
+    // ignore dotfiles, nome_modules, android and ios folders
+    ignored: /((^|[/\\])\..|__tests__|node_modules|android|ios)/,
+    ignoreInitial: false,
+    awaitWriteFinish: {
+      pollInterval: 500,
+      stabilityThreshold: 500,
+    },
+  });
+
+  watcher
+    .on('add', originalPath => {
+      console.log('\x1b[34m', `Added file: ${originalPath}`, '\x1b[0m');
+      copyAddedOrChangedFile(originalPath);
+    })
+    .on('change', originalPath => {
+      console.log('\x1b[32m', `Changed file: ${originalPath}`, '\x1b[0m');
+      copyAddedOrChangedFile(originalPath);
+    })
+    .on('unlink', originalPath => {
+      console.log('\x1b[35m', `Removed file: ${originalPath}`, '\x1b[0m');
+      unlinkDeletedFile(originalPath);
+    });
+}
+
+async function syncWithCore() {
+  try {
+    const response = await fetch(
+      'http://localhost:8081/sync-local-module?' +
+        new URLSearchParams({
+          name: packageJson.name,
+        }),
+    );
+
+    if (response.status === 404) {
+      throw new SyncError(
+        'Módulo ' + packageJson.name + ' não está instalado no projeto "core".',
+      );
+    }
+
+    if (response.status !== 200) {
+      throw FATAL_ERROR;
+    }
+
+    const corePath = await response.text();
+
+    startSync(corePath);
+  } catch (err) {
+    if (err.code === 'ECONNREFUSED') {
+      throw FATAL_ERROR;
+    }
+    throw err;
+  }
+}
+
+(async () => {
+  try {
+    await syncWithCore();
+  } catch (err) {
+    if (err instanceof SyncError) {
+      console.error('Erro: ' + err.message);
+      return;
+    }
+    console.error(err);
+  }
+})();
